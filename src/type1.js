6026cf985c6dfc142e8d00f530b4cb59aa46782a
Gio <dracogio@gmail.com>
2018-04-08 14:57:52 +0200

    Styled up widget type 1

--- src/type1.js
+++ src/type1.js
@@ -20,18 +20,68 @@ for (const div of divs) {
 
 // Create our template.
 function generateTemplate() {
+    const randomNumber = `${new Date().getTime()}${Math.floor(Math.random() * Math.floor(10000))}`;
     const template = `
-        <h3>Great this works marvelous...</h3>
-        ${getCodes(bitcoinCode)}
-        ${getCodes(stellarCode)}
+        <style>
+            .bistow-type-1 {
+                display: inline-block;
+                max-width: 300px;
+            }
+            .bistow-type-1-bitcoin${randomNumber}, .bistow-type-1-stellar${randomNumber} {
+                display: none;
+            }
+            .bistow-type-1-radio${randomNumber} {
+                display: none;
+            }
+            .bistow-type-1-radio${randomNumber} + label{
+                background-color: #fff;
+                box-shadow: inset 0 0 0 2px #202e37;
+                box-sizing: border-box;
+                cursor: pointer;
+                color: #202e37;
+                display: inline-block;
+                height: 30px;
+                line-height: 30px;
+                font-size: 20px;
+                font-family: sans-serif;
+                text-align: center;
+                text-shadow: 0 2px 4px rgba(0,0,0,.25);
+                width: 48%;
+            }
+            .bistow-type-1-radio${randomNumber} + label:hover{
+                box-shadow: inset 0 0 0 2px #75b9be;
+                color: #75B9BE;
+                transition: all 0.4s;
+            }
+            .bistow-type-1-radio${randomNumber}:checked + label{
+                background-color: #202e37;
+                color: #fff;
+            }
+            .bistow-type-1-radio-bitcoin${randomNumber}:checked ~ .bistow-type-1-bitcoin${randomNumber} {
+                display:block;
+            }
+            .bitstow-type-1-radio-stellar${randomNumber}:checked ~ .bistow-type-1-stellar${randomNumber} {
+                display:block;
+            }
+        </style>
+        <input class="bistow-type-1-radio-bitcoin${randomNumber} bistow-type-1-radio${randomNumber}" 
+            name="bistow-type-1-group${randomNumber}" 
+            id="bistow-type-1-radio-bitcoin${randomNumber}" type="radio" checked="checked" />
+        <label class="label" for="bistow-type-1-radio-bitcoin${randomNumber}">Bitcoin</label>
+        <input class="bitstow-type-1-radio-stellar${randomNumber} bistow-type-1-radio${randomNumber}" 
+            name="bistow-type-1-group${randomNumber}" 
+            id="bitstow-type-1-radio-stellar${randomNumber}" type="radio" />
+        <label class="label" for="bitstow-type-1-radio-stellar${randomNumber}">Stellar</label>
+        ${getCodes(bitcoinCode, `bistow-type-1-bitcoin${randomNumber}`)}
+        ${getCodes(stellarCode, `bistow-type-1-stellar${randomNumber}`)}
     `;
     return template;
 }
 
 // Helper function for the template, if the code exists display it otherwise return nothing and skip it.
-function getCodes(qrCode) {
+function getCodes(qrCode, className) {
     if (!qrCode) {
         return '';
     }
-    return `<img src="${qrCode}" />`;
+    return `<img src="${qrCode}" class="${className}"/>`;
 };
